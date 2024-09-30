cee1f198ed6b5673ee7263d45c8e2b59af22019b
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-09-30 12:23:28 +0900

    見やすいように実行結果を調整

--- main.cpp
+++ main.cpp
@@ -1,29 +1,41 @@
 #include <stdio.h>
 
-// 関数テンプレート
-template<typename T>
-T min(T a, T b) {
-   if (a < b) {
-       return a;
-   }
-   else {
-       return b;
-   }
+/// <summary>
+/// 再帰関数
+/// </summary>
+float Recursive(int hours, float wage) {
+    // もし時間が0なら、賃金は0
+    if (hours == 0) {
+        return 0.0;
+    }
+    // 次の時間の賃金を計算し、再帰的に次の時間の賃金を計算する
+    return wage + Recursive(hours - 1, (wage * 2) - 50);
 }
-// テンプレートの特殊化（オーバーライド）
-template <>
-char min<char>(char a, char b) {
 
-   printf("数字以外は入力できません");
-   return '\0';
+/// <summary>
+/// 一般的な賃金体系の計算を行う関数
+/// </summary>
+float General(int hours) {
+    const float hourly_wage = 1072.0;
+    return hourly_wage * hours;
 }
 
 int main() {
+    
+    printf("時間  | 一般賃金      | 再帰賃金      | 結果\n");
+    printf("-----------------------------------------------\n");
 
-    printf("%d\n", min<int>(115, 112));              // int
-    printf("%f\n", min<float>(1.2f, 1.19f));         // float
-    printf("%lf\n", min<double>(5.2f, 5.1999f));     // double
-    printf("%c\n", min<char>('a','あ'));             // char
+    for (int hours = 1; hours <= 10; hours++) {
+        float generalWages = General(hours);
+        float recursiveWages = Recursive(hours, 100.0);
+        printf("%-4d | %-12.0f | %-12.0f | ", hours, generalWages, recursiveWages);
+        if (recursiveWages > generalWages) {
+            printf("再帰賃金が高い\n");
+        }
+        else {
+            printf("一般賃金が高い\n");
+        }
+    }
 
     return 0;
 }
