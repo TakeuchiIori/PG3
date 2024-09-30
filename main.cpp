0b283bed74399f5f67d37cc331ed811b220b1a61
タケウチイオリ <146906586+TakeuchiIori@users.noreply.github.com>
2024-09-30 10:42:50 +0900

    文字列を出力

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
+    
+    // ポケモンの名前を設定
+    char pokemon1[] = "ピカチュウ";
+    char pokemon2[] = "イーブイ";
+    char pokemon3[] = "ミュウツー";
+   
+    // ポケモンの名前を出力
+    printf("%s\n", pokemon1);
+    printf("%s\n", pokemon2);
+    printf("%s\n", pokemon3);
 
-	return 0;
+    return 0;
 }
